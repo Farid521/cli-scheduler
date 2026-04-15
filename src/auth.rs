b303a8376b68036936139d807292d1827f9ed8a4
farid <faritfir521@gmail.com>
2026-04-15 19:50:18 +0700

    using struct instead of hashmap on post request form

--- src/auth.rs
+++ src/auth.rs
@@ -16,22 +16,28 @@ fn get_token(code: &str) -> Result<(), Box<dyn std::error::Error>>{
     let res_client = Client::new();
 
     let client_id = env::var("CLIENT_ID")?;
-    let redirect_url = env::var("REDIRECT_URI")?;
+    let redirect_uri = env::var("REDIRECT_URI")?;
     let client_secret = env::var("CLIENT_SECRET")?;
 
-    let mut auth_header = std::collections::HashMap::new();
-    auth_header.insert("code", code);
-    auth_header.insert("client_id", &client_id);
-    auth_header.insert("client_secret", &client_secret);
-    auth_header.insert("redirect_uri", &redirect_url);
-    auth_header.insert("grant_type", "authorization_code");
+    let request_header = TokenRequest {
+        client_id,
+        client_secret,
+        code: code.to_string(),
+        grant_type: "authorization_code".to_string(),
+        redirect_uri
+    };
+
+    // let mut auth_header = std::collections::HashMap::new();
+    // auth_header.insert("code", code);
+    // auth_header.insert("client_id", &client_id);
+    // auth_header.insert("client_secret", &client_secret);
+    // auth_header.insert("redirect_uri", &redirect_url);
+    // auth_header.insert("grant_type", "authorization_code");
 
     let res = res_client
-        .post("")
-        .form(&auth_header)
+        .post("https://oauth2.googleapis.com/token")
+        .form(&request_header)
         .send()?;
-
-
     println!("response: {}", res.text()?);
     Ok(())
 }
