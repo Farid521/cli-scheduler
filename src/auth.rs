ec8ec28f681843010b8970d2f823f5928f4560e4
farid <faritfir521@gmail.com>
2026-04-18 12:43:58 +0700

    implementing enum on custom err

--- src/auth.rs
+++ src/auth.rs
@@ -3,6 +3,45 @@ use std;
 use serde::Serialize;
 use std::env;
 
+#[derive(Debug)]
+pub enum AuthError {
+    MissingCode,
+    EnvVar(std::env::VarError),
+    Request(reqwest::Error),
+}
+
+impl std::fmt::Display for AuthError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AuthError::MissingCode => write!(f, "auth code is not provided"),
+            AuthError::EnvVar(e) => write!(f, "environment variable error: {}", e),
+            AuthError::Request(e) => write!(f, "request error: {}", e),
+        }
+    }
+}
+
+impl std::error::Error for AuthError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            AuthError::EnvVar(e) => Some(e),
+            AuthError::Request(e) => Some(e),
+            _ => None,
+        }
+    }
+}
+
+impl From<std::env::VarError> for AuthError {
+    fn from(err: std::env::VarError) -> Self {
+        AuthError::EnvVar(err)
+    }
+}
+
+impl From<reqwest::Error> for AuthError {
+    fn from(err: reqwest::Error) -> Self {
+        AuthError::Request(err)
+    }
+}
+
 #[derive(Serialize)]
 struct TokenRequest {
     client_id: String,
@@ -12,7 +51,11 @@ struct TokenRequest {
     redirect_uri: String,
 }
 
-fn get_token(code: &str) -> Result<(), Box<dyn std::error::Error>>{
+pub fn get_token(code: &str) -> Result<(), AuthError>{
+    if code == "null" {
+        return Err(AuthError::MissingCode);
+    } 
+
     let res_client = Client::new();
 
     let client_id = env::var("CLIENT_ID")?;
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,43 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+#[derive(Debug)]
+pub enum ServerError {
+    Io(std::io::Error),
+    Auth(crate::auth::AuthError),
+}
+
+impl std::fmt::Display for ServerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ServerError::Io(e) => write!(f, "IO error: {}", e),
+            ServerError::Auth(e) => write!(f, "Auth error: {}", e),
+        }
+    }
+}
+
+impl std::error::Error for ServerError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ServerError::Io(e) => Some(e),
+            ServerError::Auth(e) => Some(e),
+        }
+    }
+}
+
+impl From<std::io::Error> for ServerError {
+    fn from(err: std::io::Error) -> Self {
+        ServerError::Io(err)
+    }
+}
+
+impl From<crate::auth::AuthError> for ServerError {
+    fn from(err: crate::auth::AuthError) -> Self {
+        ServerError::Auth(err)
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), ServerError> {
     println!("client connected");
     let mut buff = [0; 1024];
     let data_length = stream.read(&mut buff)?;
@@ -25,7 +61,7 @@ fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     Ok(())
 }
 
-fn start_server() -> std::io::Result<()> {
+fn start_server() -> Result<(), ServerError> {
     let listener = TcpListener::bind("127.0.0.1:8090")?;
 
     let (mut stream, _) = listener.accept()?;
@@ -52,10 +88,13 @@ fn start_server() -> std::io::Result<()> {
     stream.flush()?;
 
     println!("code: {:?}", code);
+
+    crate::auth::get_token(&code.unwrap_or("null".to_string()))?;
+
     Ok(())
 }
 
-pub fn create_server() -> std::io::Result<()> {
+pub fn create_server() -> Result<(), ServerError> {
     start_server()?;
     println!("tcp server started in background");
     Ok(())
